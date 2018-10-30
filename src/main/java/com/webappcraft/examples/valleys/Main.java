e82ea6409ec2d22e23431c470e59be9c6a506d46
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-10-30 01:21:00 +0100

    Add base Main class

---
+++ src/main/java/com/webappcraft/examples/valleys/Main.java
@@ -0,0 +1,9 @@
+package com.webappcraft.examples.valleys;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO solve https://www.hackerrank.com/challenges/counting-valleys/problem
+    }
+
+}
