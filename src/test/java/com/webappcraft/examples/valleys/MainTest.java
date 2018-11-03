cb7571a368a4d4657f85d64003c49100450ee341
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-11-03 01:45:35 +0100

    Refactor tests to use parametrized form

--- src/test/java/com/webappcraft/examples/valleys/MainTest.java
+++ src/test/java/com/webappcraft/examples/valleys/MainTest.java
@@ -1,118 +1,39 @@
 package com.webappcraft.examples.valleys;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @Test
-    void shouldReturn_1_ForPath_DDUUUUDD() {
-        String input = "DDUUUUDD";
-        Integer expected = 1;
-
-        Integer result = Main.valleyCounting(input);
-
-        assertEquals(expected, result);
+    @ParameterizedTest(name = "{1} for {0} [{index}]")
+    @DisplayName("Should return valley count for given path")
+    @CsvSource({
+            "DDUUUUDD, 1",
+            "DDUDUDUDUU, 1",
+            "DUDUDU, 3",
+            "DDDUUDUUUUUDDDDU, 2",
+            ", 0",
+            "'', 0",
+            "DDDDDD, 0",
+            "UUUU, 0",
+            "DDUDDUDD, 0",
+            "UDUD, 0",
+    })
+    void positiveScenario(String path, Integer expectedValleyCount) {
+        Integer result = Main.valleyCounting(path);
+        assertEquals(expectedValleyCount, result);
     }
 
-    @Test
-    void shouldReturn_1_ForPath_DDUDUDUDUU() {
-        String input = "DDUDUDUDUU";
-        Integer expected = 1;
-
-        Integer result = Main.valleyCounting(input);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldReturn_3_ForPath_DUDUDU() {
-        String input = "DUDUDU";
-        Integer expected = 3;
-
-        Integer result = Main.valleyCounting(input);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldReturn_2_ForPath_DDDUUDUUUUUDDDDU() {
-        String input = "DDDUUDUUUUUDDDDU";
-        Integer expected = 2;
-
-        Integer result = Main.valleyCounting(input);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldReturn_0_ForNullText() {
-        String input = null;
-        Integer expected = 0;
-
-        Integer result = Main.valleyCounting(input);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldReturn_0_ForEmptyText() {
-        String input = "";
-        Integer expected = 0;
-
-        Integer result = Main.valleyCounting(input);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldThrowException_WhenInvalidText() {
-        String input = "DUXYZUD";
-
-        assertThrows(RuntimeException.class, () -> Main.valleyCounting(input));
-//        try {
-//            Integer result = ;
-//            fail("Should throw exception");
-//        } catch(RuntimeException e) {
-//              // success
-//        }
-    }
-
-    @Test
-    void shouldReturn_0_WhenTextContainsOnlyD() {
-        String input = "DDDDDD";
-        Integer expected = 0;
-
-        Integer result = Main.valleyCounting(input);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldReturn_0_WhenTextContainsOnlyU() {
-        String input = "UUUU";
-        Integer expected = 0;
-
-        Integer result = Main.valleyCounting(input);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldReturn_0_WhenText_DDUDDUDD() {
-        String input = "DDUDDUDD";
-        Integer expected = 0;
-
-        Integer result = Main.valleyCounting(input);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldReturn_0_WhenText_UDUD() {
-        String input = "UDUD";
-        Integer expected = 0;
-
-        Integer result = Main.valleyCounting(input);
-        assertEquals(expected, result);
+    @ParameterizedTest(name = "{0} [{index}]")
+    @DisplayName("Should throw exception when path contains invalid characters")
+    @CsvSource({
+            "DUXYZUD",
+    })
+    void shouldThrowException_WhenInvalidText(String path) {
+        assertThrows(RuntimeException.class, () -> Main.valleyCounting(path));
     }
 
 }
